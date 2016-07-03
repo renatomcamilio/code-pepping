2793ec931e25f05bbe5e3945af5b92824a54a80b
Renato Camilio <renatomcamilio@gmail.com>
2016-07-03 14:18:54 -0700

    Staircase problem solved

---
+++ staircase/index.js
@@ -0,0 +1,77 @@
+/**
+ * 
+
+Consider a staircase of size n = 4:
+
+   #
+  ##
+ ###
+####
+Observe that its base and height are both equal to , and the image is drawn using # symbols and spaces. The last line is not preceded by any spaces.
+
+Write a program that prints a staircase of size .
+
+Input Format
+
+A single integer, , denoting the size of the staircase.
+
+Output Format
+
+Print a staircase of size  using # symbols and spaces.
+
+Note: The last line must have  spaces in it.
+
+Sample Input
+
+6 
+Sample Output
+
+     #
+    ##
+   ###
+  ####
+ #####
+######
+Explanation
+
+The staircase is right-aligned, composed of # symbols and spaces, and has a height and width of .
+
+ */
+
+function padding(n) {
+  var result = function (n, str) {
+    if (n === 0) return str;
+    return result(n - 1, str + '-');
+  };
+
+  return result(n, '');
+}
+
+function hash(n) {
+  var result = function (n, str) {
+    if (n === 0) return str;
+    return result(n - 1, str + '#');
+  };
+
+  return result(n, '');
+}
+
+function line(current, max) {
+  // 2, 4 == --##
+  // 3, 4 == -###
+  return padding(max - current) + hash(current) + '\n';
+}
+
+function staircase(n) {
+  var result = function (current, max, stairs) {
+    if (current == max) return stairs;
+    return stairs + result(current + 1, max, line(current + 1, max));
+  };
+
+  return result(0, n, '');
+}
+
+
+console.log(
+  '\n' + staircase(6)
+);
