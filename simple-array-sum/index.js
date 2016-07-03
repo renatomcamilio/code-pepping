b858aa4f8db8e343a1e09cdabb1b18ed64c6764f
Renato Camilio <renatomcamilio@gmail.com>
2016-07-03 14:19:09 -0700

    Simple Array Sum problem solved

---
+++ simple-array-sum/index.js
@@ -0,0 +1,56 @@
+/**
+ * 
+ 
+Given an array of  integers, can you find the sum of its elements?
+
+Input Format
+
+The first line contains an integer, , denoting the size of the array. 
+The second line contains  space-separated integers representing the array's elements.
+
+Output Format
+
+Print the sum of the array's elements as a single integer.
+
+Sample Input
+
+6
+1 2 3 4 10 11
+Sample Output
+
+31
+Explanation
+
+We print the sum of the array's elements, which is: .
+
+ */
+
+/**
+6
+1 2 3 4 10 11
+
+31
+*/
+
+function parse(input) {
+  var inputLines = input.split('\n');
+  
+  return {
+    size: Number(inputLines[0]),
+    integersArray: inputLines[1].split(' ').map(Number)
+  };
+}
+
+function sum(list) {
+  var iterator = function (total, list) {
+    if (list.length === 0) return total;
+    return iterator(list.shift() + total, list);
+  };
+  
+  return iterator(0, list);
+}
+
+var testinput = '6\n1 2 3 4 10 11';
+
+var integers = parse(testinput).integersArray;
+console.log(sum(integers));
